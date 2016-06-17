a6a0dd56541bf9b9e64678446a61b1e148375f71
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2016-06-17 15:03:43 +0300

    add toggle class function

--- source/elements/common/common.js
+++ source/elements/common/common.js
@@ -12,3 +12,20 @@ var removeClass = function (element, className) {
 	else
 		element.className = element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
 };
+
+var toggleClass = function (element, className) {
+
+	if (element.classList) {
+		element.classList.toggle(className);
+	} else {
+		var classes = element.className.split(' ');
+		var existingIndex = classes.indexOf(className);
+
+		if (existingIndex >= 0)
+			classes.splice(existingIndex, 1);
+		else
+		classes.push(className);
+
+		element.className = classes.join(' ');
+	}
+};
