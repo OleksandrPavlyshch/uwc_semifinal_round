32c803e0e4d846598d3b886091d0fadc56ac1c6d
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2016-06-17 11:46:08 +0300

    Add accounting bot padding

--- source/elements/parallax/parallax.js
+++ source/elements/parallax/parallax.js
@@ -23,13 +23,13 @@ function scrollParallax(params) {
 
 	window.addEventListener('scroll', function() {
 		var scrollPosition = window.pageYOffset || document.documentElement.scrollTop
-			, elementHeight = settings.element.clientHeight
+			, elementHeight = settings.takePaddings ? settings.element.clientHeight : 0
 			, containerOffset = settings.container.offsetTop
-			, containerTopPadding = settings.takePaddings ? Math.round(parseInt(getComputedStyle(settings.container).paddingTop))-30 : 0
+			, containerTopPadding = settings.takePaddings ? Math.round(parseInt(getComputedStyle(settings.container).paddingTop)) : 0
 			, containerBotPadding = settings.takePaddings ? Math.round(parseInt(getComputedStyle(settings.container).paddingBottom)) : 0
 			, containerOutHeight = settings.container.offsetHeight
 			, startPoint = containerOffset + containerTopPadding
-			, endPoint = containerOffset + containerOutHeight
+			, endPoint = containerOffset + containerOutHeight - elementHeight - containerBotPadding
 			, parallaxValue = scrollPosition - containerOffset - containerTopPadding;
 
 			// console.log(containerTopPadding);
